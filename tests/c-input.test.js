1f0727fc721b863beb4ca76b49ef850a2c0ac57b
Steeve Payraudeau <16964532+steevepay@users.noreply.github.com>
2024-03-21 14:55:54 +0400

    Add tests input

---
+++ tests/c-input.test.js
@@ -0,0 +1,26 @@
+import { assert, describe, it } from 'vitest'
+import render from '@riotjs/ssr'
+
+import cInput from '../components/c-input.riot'
+
+describe('Component c-input', () => {
+    it('should render the input without props', () => {
+        const html = render('c-input', cInput, {})
+    
+        assert.strictEqual(html, '<c-input><div class="field border"><input value="" type="text"></div></c-input>')
+    })
+
+    it('should render the input with a default value', () => {
+        const _htmlExpected = '<c-input><div class="field border"><input value="Firstname" type="text"></div></c-input>';
+        const _html = render('c-input', cInput, { value: "Firstname"})
+    
+        assert.strictEqual(_html, _htmlExpected)
+    })
+
+    it('should render multiple props: label, type, error and round', () => {
+        const _htmlExpected = '<c-input><div class="field border round invalid label"><input value="" type="password"><label>Password</label><span class="error">The password is too show, minimum 20 characters.</span></div></c-input>';
+        const _html = render('c-input', cInput, { label: "Password", type: "password", error: "The password is too show, minimum 20 characters.", round: true})
+    
+        assert.strictEqual(_html, _htmlExpected)
+    })
+});
--- vite.config.mjs
+++ vite.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import riot from 'rollup-plugin-riot'
 
 export default defineConfig({
-  root       : process.cwd() + '/client',
   plugins    : [riot()],
   build: { 
     minify       : 'esbuild',
     target       : 'esnext'
-  }
+  },
+  test: {
+    environment: 'node'
+  },
 })
