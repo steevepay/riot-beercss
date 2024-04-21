2efb5dc003eacefc747fccbb91e11f359bfca810
Steeve Payraudeau <16964532+steevepay@users.noreply.github.com>
2024-04-21 12:07:33 +0400

    Riot Meiosis

---
+++ examples/meiosis/store.js
@@ -0,0 +1,31 @@
+
+import { RiotMeiosis } from '@riot-tools/meiosis';
+
+const state = {
+  firstname     : 'John',
+  lastname      : 'Wick',
+  firstnameEdit : '',
+  lastnameEdit  : '',
+  email         : '',
+  displayForm   : false
+}
+
+
+/** Create the state manager instance */
+const stateManager = new RiotMeiosis(state, { flushOnRead: false, statesToKeep: 1 });
+
+/** Extract the state stream **/
+const { stream } = stateManager;
+
+/** Add Root state reducer: merge the old and new state objects */
+stream.addReducer((newState, oldState) => {
+    // Object oldState { firstname: "John", lastname: "Wick", firstnameEdit: "", lastnameEdit: "", email: "", displayForm: false } 
+    // Object newState { displayForm: true }
+    return {
+        ...oldState,
+        ...newState
+    }
+});
+
+export default stateManager;
+
