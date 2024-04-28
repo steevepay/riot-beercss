bfa78b3123def9a43f92ef58d2591d97c23049b8
Steeve Payraudeau <16964532+steevepay@users.noreply.github.com>
2024-04-28 14:29:14 +0400

    Simplify advanced example

--- examples/riot-route/routes.js
+++ examples/riot-route/routes.js
@@ -3,31 +3,37 @@ export default [
       name     : 'Inbox',
       href     : '/',
       path     : '/(/?[?#].*)?(#.*)?',
-      component: 'p-inbox'
+      component: 'p-inbox',
+      icon     : 'inbox'
     },
     {
       name     : 'Starred',
       href     : '/favorite',
       path     : '/favorite(/?[?#].*)?(#.*)?',
-      component: 'p-favorite'
+      component: 'p-favorite',
+      icon     : 'favorite'
     },
     {
       name     : 'Sent',
       href     : '/sent',
       path     : '/sent(/?[?#].*)?(#.*)?',
-      component: 'p-sent'
+      component: 'p-sent',
+      icon     : 'send',
+      separator: true
     },
     {
         name     : 'Subscription',
         href     : '/subscription',
         path     : '/subscription(/?[?#].*)?(#.*)?',
-        component: 'p-subscription'
+        component: 'p-subscription',
+        icon     : 'rocket',
     },
     {
       name     : 'Settings',
       href     : '/settings',
       path     : '/settings(/?[?#].*)?(#.*)?',
-      component: 'p-settings'
+      component: 'p-settings',
+      icon     : 'settings'
     }
   ]
   
