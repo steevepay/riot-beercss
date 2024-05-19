c9e614ef6fae1289c3a10017124cdb0047069317
Steeve Payraudeau <16964532+steevepay@users.noreply.github.com>
2024-05-19 23:39:45 +0400

    pre build riot components

---
+++ components/index.js
@@ -0,0 +1,38 @@
+import { version } from '../package.json';
+
+import cBadge from "./c-badge.riot"
+import cButtonGroup from "./c-button-group.riot"
+import cButton from "./c-button.riot"
+import cCheckbox from "./c-checkbox.riot"
+import cChip from "./c-chip.riot"
+import cDialog from "./c-dialog.riot"
+import cIcon from "./c-icon.riot"
+import cInput from "./c-input.riot"
+import cRadioGroup from "./c-radio-group.riot"
+import cRadio from "./c-radio.riot"
+import cSearch from "./c-search.riot"
+import cSelect from "./c-select.riot"
+import cSnackbar from "./c-snackbar.riot"
+import cSwitch from "./c-switch.riot"
+import cTable from "./c-table.riot"
+import cTabs from "./c-tabs.riot"
+
+export {
+    version,
+    cBadge,
+    cButtonGroup,
+    cButton,
+    cCheckbox,
+    cChip,
+    cDialog,
+    cIcon,
+    cInput,
+    cRadioGroup,
+    cRadio,
+    cSearch,
+    cSelect,
+    cSnackbar,
+    cSwitch,
+    cTable,
+    cTabs
+};
