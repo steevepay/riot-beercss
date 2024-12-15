d8657069e0cb966666e793c2dee34ce7932a5605
Steeve Payraudeau <16964532+steevepay@users.noreply.github.com>
2024-12-15 12:47:24 +0100

    Update structure

---
+++ routes.js
@@ -0,0 +1,87 @@
+export default {
+  pages: [
+    {
+      name: "Home",
+      href: "/",
+      path: "/(/?[?#].*)?(#.*)?",
+      component: "p-home",
+      icon: "home",
+    }
+  ],
+  components: [
+    {
+        name: "Badges",
+        href: "/#badges",
+        path: "/#badges",
+        section: 's-badges'
+    },
+    {
+        name: "Buttons",
+        href: "/#buttons",
+        path: "/#buttons",
+        section: 's-buttons'
+    },
+    {
+        name: "Checkbox",
+        href: "/#checkbox",
+        path: "/#checkbox",
+        section: 's-checkboxes'
+    },
+    {
+        name: "Chips",
+        href: "/#chips",
+        path: "/#chips",
+        section: 's-chips'
+    },
+    {
+        name: "Dialogs",
+        href: "/#dialogs",
+        path: "/#dialogs"
+    },
+    {
+        name: "Icon",
+        href: "/#icon",
+        path: "/#icon"
+    },
+    {
+        name: "Inputs",
+        href: "/#inputs",
+        path: "/#inputs"
+    },
+    {
+        name: "Radios",
+        href: "/#radios",
+        path: "/#radios"
+    },
+    {
+        name: "Switches",
+        href: "/#switches",
+        path: "/#switches"
+    },
+    {
+        name: "Snackbars",
+        href: "/#snackbars",
+        path: "/#snackbars"
+    },
+    {
+        name: "Selects",
+        href: "/#selects",
+        path: "/#selects"
+    },
+    {
+        name: "Search",
+        href: "/#search",
+        path: "/#search"
+    },
+    {
+        name: "Tabs",
+        href: "/#tabs",
+        path: "/#tabs"
+    },
+    {
+        name: "Tables",
+        href: "/#tables",
+        path: "/#tables"
+    },
+  ]
+};
